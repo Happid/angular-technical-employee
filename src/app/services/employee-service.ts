ff158e930a0e6297b126b94ef58f72c9a66ad1a4
Happid <happidilmi@gmail.com>
2025-12-25 11:43:03 +0700

    feat: add employee mdel and service for get the data employee

---
+++ src/app/services/employee-service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Employee } from '../models/employee.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class EmployeeService {
+  
+  private readonly DATA_URL = 'assets/data/employees.json';
+
+  constructor(private http: HttpClient) {}
+
+  getEmployees(): Observable<Employee[]> {
+    return this.http.get<Employee[]>(this.DATA_URL);
+  }
+  
+}
